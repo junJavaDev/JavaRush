8732b3ab0cbd240e2a28f9a59eaea03c645ee3b1
junJavaDev <ogarkov.pro@gmail.com>
2022-01-08 16:53:50 +0300

    add Tasks 25 LVL, del dublicated classes

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution (2).java
+++
@@ -1,9 +0,0 @@
-package com.javarush.task.pro.task02.task0206;
-
-public class Solution {
-
-    public static void main(String[] args) {
-        String word = "Слово", phrase = "Фраза", line = "Линия", text = "Текст";
-
-    }
-}
