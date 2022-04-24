8e1207c8878eb9872fad7a636c2c4699cf0e3a1e
junJavaDev <ogarkov.pro@gmail.com>
2022-04-24 23:03:14 +0300

    add second module Theme 3 and 4

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task13/task1304/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.jdk13.task13.task1304;
+
+/* 
+Эй, ты там живой?
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+    public interface Person {
+        boolean isAlive ();
+    }
+    public interface Presentable extends Person {
+
+    }
+}
+
