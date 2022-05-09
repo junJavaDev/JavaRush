de48d36b441ab68b59d98c78838ff4d9c2f3646a
junJavaDev <ogarkov.pro@gmail.com>
2022-05-09 21:09:12 +0300

    add task1703 thread 12

--- 0.Notes/src/Temp/Temp.java
+++ 0.Notes/src/Temp/Temp.java
@@ -2,10 +2,14 @@ package Temp;
 
 import java.io.*;
 import java.util.Arrays;
+import java.util.concurrent.BlockingQueue;
 
 public class Temp {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(getNeighbours(5, 9)));
+
+        System.out.println(Arrays.deepToString(gameField));
+        rotateClockwise();
+        System.out.println(Arrays.deepToString(gameField));
     }
     private static int[] getNeighbours(int parent, int quantity) {
         int[] neighbours = new int[quantity];
@@ -27,6 +31,36 @@ public class Temp {
         return neighbours;
     }
 
+    public static int[][] gameField =
+            {{2, 2, 2, 2},
+            {4, 4, 4, 4},
+            {8, 8, 8, 8},
+            {0, 0, 0, 0}};
 
+
+    private static void rotateClockwise() {
+        int[][] temp =
+                {{2, 2, 2, 2},
+                {4, 4, 4, 4},
+                {8, 8, 8, 8},
+                {0, 0, 0, 0}};
+//        int[][] temp = new int[4][4];
+//        for (int i = 0; i < 4; i++) {
+//            System.arraycopy(gameField[i], 0, temp[i], 0, 4);
+//        }
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                gameField[i][j] = temp[4-1-j][i];
+            }
+        }
+    }
+
+    public static class Lol implements Runnable {
+
+        @Override
+        public void run() {
+
+        }
+    }
 
 }
