20ece9cd00827758e433bb393913329326ff7076
junJavaDev <ogarkov.pro@gmail.com>
2022-06-09 22:29:41 +0300

    add Animal methods

--- src/main/java/ru/javarush/ogarkov/islandsimulation/item/abstracts/Animal.java
+++ src/main/java/ru/javarush/ogarkov/islandsimulation/item/abstracts/Animal.java
@@ -1,6 +1,24 @@
 package ru.javarush.ogarkov.islandsimulation.item.abstracts;
 
-import ru.javarush.ogarkov.islandsimulation.item.abstracts.BasicItem;
-
 public abstract class Animal extends BasicItem {
+
+    public void Eat() {
+        System.out.println("Животное ест растения и/или других животных (если в их локации есть подходящая еда)");
+    }
+
+    public void Move() {
+        System.out.println("Животное передвигается (в соседние локации)");
+    }
+
+    public void Reproduce() {
+        System.out.println("Животное размножается (при наличии пары в их локации)");
+    }
+
+    public void Die() {
+        System.out.println("Животное умирает (от голода или съедено)");
+    }
+
+
+
+
 }
