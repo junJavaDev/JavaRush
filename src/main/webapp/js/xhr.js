bcfc826827e72980ccc193d81c326f2694dc3dd1
junJavaDev <ogarkov.pro@gmail.com>
2022-07-31 20:06:16 +0300

    add pages buttons logics

--- src/main/webapp/js/xhr.js
+++ src/main/webapp/js/xhr.js
@@ -1,20 +1,63 @@
-const requestListPlayersURL = '/rest/players'
-const requestAllAccountsURL = '/rest/players/count'
+const accountsURL = '/rest/players'
+const countAllAccountsURL = '/rest/players/count'
 const selectCountPerPage = document.getElementById('selectCountPerPage')
 
-sendRequest('GET', requestListPlayersURL)
-    .then(data => createTable(data))
-    .catch(err => console.log(err))
+updateTable(0, 3)
+
 
 selectCountPerPage.onchange = function () {
-    console.log(getAccountsCount())
-    selectCountPerPage.options[selectCountPerPage.selectedIndex].value
+    const countPerPage = selectCountPerPage.options[selectCountPerPage.selectedIndex].value;
+    updateTable(0, countPerPage)
+
 }
 
+// function setSomeString(newStr) {
+//     someString = newStr
+//     console.log(someString)
+// }
+
+
+function updateTable(pageNumber, countPerPage) {
+    getAccountsCount()
+        .then(accountsCount => {
+            let pages = Math.ceil(accountsCount / countPerPage)
+            updatePagesButton(pages, pageNumber, countPerPage)
+            console.log(pages)
+            return getAccountsData(pageNumber, countPerPage)
+        })
+        .then(data => drawTable(data))
+        .catch(err => console.log(err))
+}
+
+function updatePagesButton(pages, pageNumber, countPerPage) {
+    const pageButtons = document.getElementById('pageButtons')
+        let buttons = [];
+        buttons.push(document.createTextNode('Pages:'))
+        for (let buttonNumber = 1; buttonNumber <= pages; buttonNumber++) {
+            const button = document.createElement('button')
+            const buttonText = document.createTextNode('' + buttonNumber);
+            if (pageNumber + 1 === buttonNumber) {
+                button.style.background = "darkseagreen";
+            }
+            button.appendChild(buttonText);
+            button.onclick = function () {
+                updateTable(buttonNumber - 1, countPerPage)
+            };
+            buttons.push(button)
+        }
+        pageButtons.replaceChildren(...buttons)
+}
 
 function getAccountsCount() {
-    const countPromise = sendRequest('GET', requestAllAccountsURL)
-    return countPromise.then()
+    return sendRequest('GET', countAllAccountsURL)
+}
+
+function getAccountsData(pageNumber, countPerPage) {
+    return sendRequest('GET', getPlayersURLWithParam(pageNumber, countPerPage))
+}
+
+function getPlayersURLWithParam(pageNumber, pageSize) {
+    return accountsURL + "?" + "pageNumber=" + pageNumber + "&" + "pageSize=" + pageSize
 }
 
 function sendRequest(method, url, body = null) {
@@ -37,21 +80,34 @@ function sendRequest(method, url, body = null) {
     });
 }
 
-function createTable(data) {
-    const body = document.getElementsByTagName("body")[0];
-    const table = document.getElementById('table');
-    const tbody = document.createElement('tbody');
-    for (const dataKey in data) {
-        const rowObject = data[dataKey];
+function drawTable(data) {
+    const tbody = document.getElementById('tableBody')
+    let rows = [];
+    for (const rowIndex in data) {
+        const rowData = data[rowIndex];
         const row = document.createElement("tr");
-        for (const key in rowObject) {
-            const td = document.createElement("td");
-            const cellText = document.createTextNode(rowObject[key]);
-            td.appendChild(cellText);
-            row.appendChild(td);
-        }
-        tbody.appendChild(row);
+        const birthday = new Date(rowData['birthday'])
+            .toLocaleString('en', {
+                day: 'numeric',
+                month: 'numeric',
+                year: 'numeric'
+            });
+        row.appendChild(createTd(rowData['id']));
+        row.appendChild(createTd(rowData['name']));
+        row.appendChild(createTd(rowData['title']));
+        row.appendChild(createTd(rowData['race']));
+        row.appendChild(createTd(rowData['profession']));
+        row.appendChild(createTd(rowData['level']));
+        row.appendChild(createTd(birthday));
+        row.appendChild(createTd(rowData['banned']));
+        rows.push(row)
     }
-    body.appendChild(table);
-    table.appendChild(tbody);
+    tbody.replaceChildren(...rows)
+}
+
+function createTd(text) {
+    const td = document.createElement("td");
+    const cellText = document.createTextNode(text);
+    td.appendChild(cellText)
+    return td
 }
