b9d2f9333d395e7b09e9cad12717bb8c3539f160
junJavaDev <ogarkov.pro@gmail.com>
2022-08-07 22:35:18 +0300

    all complete (edit webConfig)

--- src/main/java/com/game/config/WebConfig.java
+++ src/main/java/com/game/config/WebConfig.java
@@ -50,7 +50,6 @@ public class WebConfig implements WebMvcConfigurer, ApplicationContextAware {
         registry.addResourceHandler("/css/**").addResourceLocations("/css/");
         registry.addResourceHandler("/img/**").addResourceLocations("/img/");
         registry.addResourceHandler("/js/**").addResourceLocations("/js/");
-        registry.addResourceHandler("/fonts/**").addResourceLocations("/fonts/");
     }
 
     @Bean
