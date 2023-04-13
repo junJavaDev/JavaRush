aa3b2e8b3b293892200addc570fb10116729a3f6
Alexander Ogarkov <ogarkov.pro@gmail.com>
2023-04-13 12:51:40 +0300

    Task 4207 CountDownLatch done

---
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task42/task4202/Solution.java
@@ -0,0 +1,50 @@
+package com.javarush.task.jdk13.task42.task4202;
+
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/* 
+Кто первый встал, того и тапки
+*/
+
+public class Solution {
+    private static final int COUNT = 5;
+    private static final int TIMEOUT = 10;
+    private static AtomicBoolean isHit = new AtomicBoolean(false);
+
+    public static void main(final String[] arguments) throws InterruptedException {
+        ExecutorService executor = Executors.newFixedThreadPool(COUNT);
+
+        for (int i = 1; i <= COUNT; i++) {
+            Solution.Shooter shooter = new Solution.Shooter(i);
+            executor.submit(shooter);
+        }
+
+        executor.awaitTermination(TIMEOUT, TimeUnit.MILLISECONDS);
+
+        executor.shutdown();
+    }
+
+    static class Shooter implements Runnable {
+        private int id;
+
+        public Shooter(int id) {
+            this.id = id;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Did someone hit? " + isHit);
+            if (!isHit.get()) {
+                System.out.println("The shooter #" + id + " hit!");
+                isHit.set(true);
+                System.out.println("Did someone hit? " + isHit);
+            }
+        }
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task42/task4205/Message.java
@@ -0,0 +1,38 @@
+package com.javarush.task.jdk13.task42.task4205;
+
+import java.sql.Time;
+import java.time.Duration;
+import java.util.concurrent.*;
+
+public class Message implements Delayed {
+    private Integer messageId;
+    private String messageText;
+    private long lifetime;
+    private long endTime;
+
+    public Message(Integer messageId, String messageText, long lifetime) {
+        this.messageId = messageId;
+        this.messageText = messageText;
+        this.lifetime = lifetime;
+        this.endTime = System.nanoTime() + TimeUnit.NANOSECONDS.convert(lifetime, TimeUnit.MILLISECONDS);
+    }
+
+    public Integer getMessageId() {
+        return messageId;
+    }
+
+    public long getLifetime() {
+        return lifetime;
+    }
+
+    @Override
+    public long getDelay(TimeUnit unit) {
+        return unit.convert(endTime - System.nanoTime(), TimeUnit.NANOSECONDS);
+    }
+
+    @Override
+    public int compareTo(Delayed o) {
+        long d = getDelay(TimeUnit.NANOSECONDS) - o.getDelay(TimeUnit.NANOSECONDS);
+        return (d == 0) ? 0 : ((d > 0) ? 1 : -1);
+    }
+}
