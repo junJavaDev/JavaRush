4dbdb0c347bcfd10888b2c58a898f8176fa9ccee
Alexander Ogarkov <ogarkov.pro@gmail.com>
2023-04-14 14:44:40 +0300

    Task 1102 done

--- 3.JavaMultithreading/src/com/javarush/task/sql/task11/task1103/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task11/task1103/Solution.java
@@ -1,36 +1,37 @@
-package com.javarush.task.sql.task11.task1102;
+package com.javarush.task.sql.task11.task1103;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 /* 
-task1102
+task1103
 */
 
 public class Solution {
+    public static Animal animalGet;
+    public static Animal animalLoad;
+    public static Animal animalFind;
+
     public static void main(String[] args) throws Exception {
         Animal animalCat = new Animal();
         animalCat.setName("Murka");
         animalCat.setAge(5);
         animalCat.setFamily("Cats");
 
-        Animal animalDog = new Animal();
-        animalDog.setName("Barsik");
-        animalDog.setAge(3);
-        animalDog.setFamily("Dogs");
-
         try {
             SessionFactory sessionFactory = MySessionFactory.getSessionFactory();
             Session session = sessionFactory.openSession();
             Transaction transaction = session.beginTransaction();
-            session.save(animalCat);
-            session.saveOrUpdate(animalCat);
-            session.saveOrUpdate(animalDog);
+            long id = (long) session.save(animalCat);
+            //напишите тут ваш код
             transaction.commit();
             session.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
+        System.out.println(animalGet == null);
+        System.out.println(animalLoad != null);
+        System.out.println(animalFind == null);
     }
 }
