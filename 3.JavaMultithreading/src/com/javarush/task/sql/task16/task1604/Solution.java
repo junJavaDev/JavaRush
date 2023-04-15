507ff0f8d679583d0a845b426925f96b1e4c31b6
Alexander Ogarkov <ogarkov.pro@gmail.com>
2023-04-16 02:05:46 +0300

    Task 1604 done

--- 3.JavaMultithreading/src/com/javarush/task/sql/task16/task1604/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task16/task1604/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.sql.task16.task1604;
 
 import org.hibernate.Session;
+import org.hibernate.query.NativeQuery;
 import org.hibernate.query.Query;
 
 import java.util.List;
@@ -13,8 +14,10 @@ public class Solution {
 
     public static void main(String[] args) throws Exception {
         try (Session session = MySessionFactory.getSessionFactory().openSession()) {
-            //напишите тут ваш код
-
+            List<Object[]> list = session.createNativeQuery("SELECT id, title, created_time FROM project").list();
+            for (Object[] objects : list) {
+                System.out.printf("%s, %s, %s", objects[0], objects[1], objects[2]);
+            }
         }
     }
 }
