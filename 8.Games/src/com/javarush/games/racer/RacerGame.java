b9867be5b2e92d8675f3e7ab92c55812443a8f17
Alexander Ogarkov <ogarkov.pro@gmail.com>
2023-04-16 15:04:45 +0300

    RacerGame 3/28 done

--- 8.Games/src/com/javarush/games/racer/RacerGame.java
+++ 8.Games/src/com/javarush/games/racer/RacerGame.java
@@ -7,6 +7,9 @@ public class RacerGame extends Game {
     public static final int HEIGHT = 64;
     public static final int CENTER_X = WIDTH / 2;
     public static final int ROADSIDE_WIDTH = 14;
+    private static final Color ROADSIDE_COLOR = Color.GREEN;
+    private static final Color ROAD_COLOR = Color.GRAY;
+    private static final Color DIVIDING_STRIP_COLOR = Color.WHITE;
 
     @Override
     public void initialize() {
@@ -15,13 +18,23 @@ public class RacerGame extends Game {
         showGrid(false);
     }
 
-    private void createGame(){
+    private void createGame() {
         drawScene();
     }
-    private void drawScene(){
+
+    private void drawScene() {
         drawField();
     }
-    private void drawField(){
 
+    private void drawField() {
+        for (int w = 0; w < WIDTH; w++) {
+            for (int h = 0; h < HEIGHT; h++) {
+                if (w < ROADSIDE_WIDTH || w >= WIDTH - ROADSIDE_WIDTH) {
+                    setCellColor(w, h, ROADSIDE_COLOR);
+                } else if (w == CENTER_X) {
+                    setCellColor(w, h, DIVIDING_STRIP_COLOR);
+                } else setCellColor(w, h, ROAD_COLOR);
+            }
+        }
     }
 }
