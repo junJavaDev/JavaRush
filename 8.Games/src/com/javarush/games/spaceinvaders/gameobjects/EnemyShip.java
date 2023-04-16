26e280be4f825fa4f99a08309146d82a84e74d63
Alexander Ogarkov <ogarkov.pro@gmail.com>
2023-04-17 01:49:06 +0300

    SpaceInvadersGame 6/34 done

---
+++ 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/EnemyShip.java
@@ -0,0 +1,12 @@
+package com.javarush.games.spaceinvaders.gameobjects;
+
+
+import com.javarush.games.spaceinvaders.ShapeMatrix;
+
+public class EnemyShip extends Ship {
+
+    public EnemyShip(double x, double y) {
+        super(x, y);
+        setStaticView(ShapeMatrix.ENEMY);
+    }
+}
