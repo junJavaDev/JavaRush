6b5eb82686dd882570a0b4c0b138ef83006d707b
Alexander Ogarkov <ogarkov.pro@gmail.com>
2023-04-16 15:04:16 +0300

    RacerGame 2/28 done

--- 8.Games/src/com/javarush/games/racer/RacerGame.java
+++ 8.Games/src/com/javarush/games/racer/RacerGame.java
@@ -5,10 +5,23 @@ import com.javarush.engine.cell.*;
 public class RacerGame extends Game {
     public static final int WIDTH = 64;
     public static final int HEIGHT = 64;
+    public static final int CENTER_X = WIDTH / 2;
+    public static final int ROADSIDE_WIDTH = 14;
 
     @Override
     public void initialize() {
         setScreenSize(WIDTH, HEIGHT);
+        createGame();
         showGrid(false);
+    }
+
+    private void createGame(){
+        drawScene();
+    }
+    private void drawScene(){
+        drawField();
+    }
+    private void drawField(){
+
     }
 }
