5c5336cc4036a361e9a4c916326331e83ca30fe2
Alexander Ogarkov <ogarkov.pro@gmail.com>
2023-04-16 16:08:15 +0300

    RacerGame 16/28 done

--- 8.Games/src/com/javarush/games/racer/GameObject.java
+++ 8.Games/src/com/javarush/games/racer/GameObject.java
@@ -1,6 +1,7 @@
 package com.javarush.games.racer;
 
-import com.javarush.engine.cell.*;
+import com.javarush.engine.cell.Color;
+import com.javarush.engine.cell.Game;
 
 public class GameObject {
     public int x;
@@ -18,16 +19,55 @@ public class GameObject {
         this.x = x;
         this.y = y;
         this.matrix = matrix;
-        this.width = matrix[0].length;
-        this.height = matrix.length;
+        width = matrix[0].length;
+        height = matrix.length;
     }
 
     public void draw(Game game) {
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                game.setCellColor(this.x + x, this.y + y, Color.values()[matrix[y][x]]);
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                int colorIndex = matrix[j][i];
+                game.setCellColor(x + i, y + j, Color.values()[colorIndex]);
             }
         }
     }
 
+    public boolean isCollisionPossible(GameObject otherGameObject) {
+        if (x > otherGameObject.x + otherGameObject.width || x + width < otherGameObject.x) {
+            return false;
+        }
+
+        if (y > otherGameObject.y + otherGameObject.height || y + height < otherGameObject.y) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isCollision(GameObject gameObject) {
+        if (!isCollisionPossible(gameObject)) {
+            return false;
+        }
+
+        for (int carX = 0; carX < gameObject.width; carX++) {
+            for (int carY = 0; carY < gameObject.height; carY++) {
+                if (gameObject.matrix[carY][carX] != 0) {
+                    if (isCollision(carX + gameObject.x, carY + gameObject.y)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean isCollision(int x, int y) {
+        for (int matrixX = 0; matrixX < width; matrixX++) {
+            for (int matrixY = 0; matrixY < height; matrixY++) {
+                if (matrix[matrixY][matrixX] != 0 && matrixX + this.x == x && matrixY + this.y == y) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
--- 8.Games/src/com/javarush/games/racer/road/RoadManager.java
+++ 8.Games/src/com/javarush/games/racer/road/RoadManager.java
@@ -1,6 +1,7 @@
 package com.javarush.games.racer.road;
 
 import com.javarush.engine.cell.Game;
+import com.javarush.games.racer.PlayerCar;
 import com.javarush.games.racer.RacerGame;
 
 import java.util.ArrayList;
@@ -14,22 +15,6 @@ public class RoadManager {
     private static final int FIRST_LANE_POSITION = 16;
     private static final int FOURTH_LANE_POSITION = 44;
     private List<RoadObject> items = new ArrayList<>();
-    private boolean isThornExists() {
-        for (RoadObject item : items) {
-            if (item instanceof Thorn) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private void generateThorn(Game game) {
-        int randomNumber = game.getRandomNumber(100);
-        if (randomNumber < 10 && !isThornExists()) {
-            addRoadObject(RoadObjectType.THORN, game);
-        }
-    }
-
     public void generateNewRoadObjects(Game game) {
         generateThorn(game);
     }
@@ -45,6 +30,31 @@ public class RoadManager {
             item.move(item.speed + boost);
         }
         deletePassedItems();
+    }
+
+    public boolean checkCrush(PlayerCar playerCar) {
+        for (RoadObject item : items) {
+            if (item.isCollision(playerCar)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isThornExists() {
+        for (RoadObject item : items) {
+            if (item instanceof Thorn) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void generateThorn(Game game) {
+        int randomNumber = game.getRandomNumber(100);
+        if (randomNumber < 10 && !isThornExists()) {
+            addRoadObject(RoadObjectType.THORN, game);
+        }
     }
 
     private void deletePassedItems() {
