6988dba00d42486f7c8db8947cb464e4eaec851f
Alexander Ogarkov <ogarkov.pro@gmail.com>
2023-04-16 20:55:12 +0300

    SpaceInvadersGame 2/34 done

--- 8.Games/src/com/javarush/games/spaceinvaders/SpaceInvadersGame.java
+++ 8.Games/src/com/javarush/games/spaceinvaders/SpaceInvadersGame.java
@@ -10,5 +10,20 @@ public class SpaceInvadersGame extends Game {
     @Override
     public void initialize() {
         setScreenSize(WIDTH, HEIGHT);
+        createGame();
+    }
+
+    // Создание элементов игры
+    private void createGame() {
+        drawScene();
+    }
+    // Отрисовка элементов игры
+    private void drawScene() {
+        drawField();
+    }
+
+    // Отрисовка космоса
+    private void drawField() {
+
     }
 }
