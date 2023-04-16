8c6a1f3f02017a75d022662f5c81c2d8037094c9
Alexander Ogarkov <ogarkov.pro@gmail.com>
2023-04-17 01:20:56 +0300

    SpaceInvadersGame 4/34 done

--- 8.Games/src/com/javarush/games/spaceinvaders/SpaceInvadersGame.java
+++ 8.Games/src/com/javarush/games/spaceinvaders/SpaceInvadersGame.java
@@ -1,10 +1,15 @@
 package com.javarush.games.spaceinvaders;
 
 import com.javarush.engine.cell.*;
+import com.javarush.games.spaceinvaders.gameobjects.Star;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SpaceInvadersGame extends Game {
     public static final int WIDTH = 64;
     public static final int HEIGHT = 64;
+    private List<Star> stars;
 
 
     @Override
@@ -15,6 +20,7 @@ public class SpaceInvadersGame extends Game {
 
     // Создание элементов игры
     private void createGame() {
+        createStars();
         drawScene();
     }
     // Отрисовка элементов игры
@@ -29,5 +35,20 @@ public class SpaceInvadersGame extends Game {
                 setCellValueEx(w, h, Color.BLACK, "");
             }
         }
+        for (Star star : stars) {
+            star.draw(this);
+        }
+    }
+
+    private void createStars() {
+        stars = new ArrayList<>();
+        stars.add(new Star(3, 7));
+        stars.add(new Star(22, 33));
+        stars.add(new Star(17, 44));
+        stars.add(new Star(44, 6));
+        stars.add(new Star(33, 34));
+        stars.add(new Star(32, 7));
+        stars.add(new Star(57, 24));
+        stars.add(new Star(14, 37));
     }
 }
