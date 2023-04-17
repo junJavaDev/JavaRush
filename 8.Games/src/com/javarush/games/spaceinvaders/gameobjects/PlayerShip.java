7a2f13e813f2285b58556bf5579ab5185a8b0068
Alexander Ogarkov <ogarkov.pro@gmail.com>
2023-04-17 11:43:13 +0300

    SpaceInvadersGame 19/34 done

--- 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/PlayerShip.java
+++ 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/PlayerShip.java
@@ -25,4 +25,12 @@ public class PlayerShip extends Ship {
         }
     }
 
+    @Override
+    public void kill() {
+        if (!isAlive) {
+            return;
+        }
+        isAlive = false;
+        setAnimatedView(ShapeMatrix.KILL_PLAYER_ANIMATION_FIRST, ShapeMatrix.KILL_PLAYER_ANIMATION_SECOND, ShapeMatrix.KILL_PLAYER_ANIMATION_THIRD, ShapeMatrix.DEAD_PLAYER);
+    }
 }
