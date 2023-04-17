d458a8b014622e603f1046941d8b6387f28d9c49
Alexander Ogarkov <ogarkov.pro@gmail.com>
2023-04-17 11:00:46 +0300

    SpaceInvadersGame 13/34 done

--- 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/EnemyShip.java
+++ 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/EnemyShip.java
@@ -20,4 +20,9 @@ public class EnemyShip extends Ship {
             y += 2;
         }
     }
+
+    @Override
+    public Bullet fire() {
+        return new Bullet(x + 1, y + height, Direction.DOWN);
+    }
 }
