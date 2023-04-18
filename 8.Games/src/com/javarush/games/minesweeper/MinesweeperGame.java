77532868be8231846c29b9259afd4bdf3c581dea
Alexander Ogarkov <ogarkov.pro@gmail.com>
2023-04-18 04:52:24 +0300

    Minesweeper 4/16 done

--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -3,9 +3,23 @@ import com.javarush.engine.cell.*;
 
 public class MinesweeperGame extends Game{
     private static final int SIDE = 9;
+    private GameObject[][] gameField = new GameObject[SIDE][SIDE];
 
     @Override
     public void initialize() {
         setScreenSize(SIDE, SIDE);
+        createGame();
     }
+
+    private void createGame() {
+        for (int x = 0; x < gameField.length; x++) {
+            for (int y = 0; y < gameField[x].length; y++) {
+                gameField[y][x] = new GameObject(x, y);
+                setCellColor(x, y, Color.GRAY);
+            }
+        }
+    }
+
 }
+
+
